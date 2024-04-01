d825f0d7d075a440a0eac697a0e2bae442d60649
Sowmya200 <sowmyakumaravel2002@gmail.com>
2024-04-01 13:32:01 +0530

    login page

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:simple_shadow/simple_shadow.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({super.key});
@@ -14,7 +15,7 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
       toolbarHeight: 350, // default is 56
       toolbarOpacity: 0.15,
       backgroundColor: const Color.fromARGB(255,222,207,228),
-      //shape: CustomOvalShapeBorder(curveRadius: 0.40),
+     // shape: CustomOvalShapeBorder(curveRadius: 0.40),
 // Change the background color as needed
       flexibleSpace: ClipPath(
             clipper: WaveClipperTwo(), // Use the WaveClipperTwo custom clipper
@@ -24,12 +25,17 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
           ),    
           
     
-      title: Image.asset(
+      title: SimpleShadow(
+        color: const Color.fromARGB(255, 0, 0, 0),
+        
+        sigma: 5,
+        offset: const Offset(0, 0),
+        child: Image.asset(
           'assets/whitee_logo.png',
           fit: BoxFit.cover, // Replace this with the path to your image
           width:440.0, // Adjust the width as needed
           height:560.0, // Adjust the height as needed
-      ),),
+      ))),
       // Add more customization here
     ),);
   }
